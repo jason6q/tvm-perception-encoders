27a25c25a2fb549b4b1e283d3f8ba5ea238a5de9
nu1ptr <j6quach@gmail.com>
2025-05-29 21:28:32 -0500

    Type sigs

--- tests/utils.py
+++ tests/utils.py
@@ -1,18 +1,20 @@
 """
     Basic helper functions for testing.
 """
+from typing import Tuple, Any
+
 import tvm
 import torch
 import numpy as np
 
-def get_devices(device: str = 'cuda'):
+def get_devices(device: str = 'cuda') -> Tuple[Any, Any]:
     """
         Return a tuple with the TVM device and Torch device, useful
         when comparing between TVM and Torch modules
     """
     return (tvm.cuda(0), torch.device('cuda')) if device == 'cuda' else (tvm.cpu(0), torch.device('cpu'))
 
-def get_tensors(arr: np.ndarray, device: str = 'cpu'):
+def get_tensors(arr: np.ndarray, device: str = 'cpu') -> Tuple[tvm.nd.array, np.ndarray]:
     """
         Build a tuple with the TVM array on the correct device along with the Torch array
         on the correct device.
@@ -22,13 +24,13 @@ def get_tensors(arr: np.ndarray, device: str = 'cpu'):
     pt_arr = torch.from_numpy(arr).to(pt_device)
     return tvm_arr, pt_arr
     
-def mad(x: np.ndarray, y: np.ndarray):
+def mad(x: np.ndarray, y: np.ndarray) -> float:
     """
         Mean Absolute Difference
     """
     return abs(x - y).mean()
 
-def mse(x: np.ndarray, y: np.ndarray):
+def mse(x: np.ndarray, y: np.ndarray) -> float:
     """
         Mean Squared Error
     """
