afd1d6d8eebd8b31cba4128f63c074a55adafb43
nu1ptr <j6quach@gmail.com>
2025-06-07 00:25:00 -0500

    Test patch embedding

--- tests/test_rope.py
+++ tests/test_rope.py
@@ -1,11 +1,15 @@
 import sys
 sys.path.append('..')
 
+import tvm
 import torch
+import torch.nn.functional as F
 import numpy as np
 
 import core.vision_encoder.rope as pe_rope
-from rope import build_axial_freqs, apply_rope
+from rope import build_axial_freqs, ImagePatchEmbedding
+
+np.random.seed(42)
 
 def test_freqs() -> None:
     dim = 256
@@ -32,14 +36,24 @@ def test_freqs() -> None:
     seq_num = patch_w * patch_h
     seq = np.random.rand(1, seq_num, grid_w*grid_h)
 
-    print(f"Grid Size: {grid_w},{grid_h}")
-    print(f"Patch Size: {patch_w},{patch_h}")
+    print(f"Grid Size: {grid_h},{grid_w}")
+    print(f"Patch Size: {patch_h},{patch_w}")
     print(f"Patch Embedding Size: {patch_embed_dim}")
     print(f"Number of patches: {seq_num}")
     print(f"Axial Freq Shape: {my_rope2d_freq.shape}")
     print(f"Sequence Shape: {seq.shape}")
 
     # Test RoPE 2D applied to an embedding.
+    rope2d_lib = tvm.compile(ImagePatchEmbedding, target="llvm")
+
+    img = tvm.nd.array(np.random.uniform(size=(1,3,img_h,img_w)).astype("float32"))
+    weights = tvm.nd.array(np.random.uniform(size=(dim_head,3,patch_h, patch_w)).astype("float32"))
+    out = tvm.nd.array(np.zeros((1,dim_head,grid_h,grid_w), dtype="float32"))
+    rope2d_lib(img, weights, out)
+    out_pt = F.conv2d(torch.tensor(img.numpy()), torch.tensor(weights.numpy()), stride=(patch_h,patch_w))
+    print("Image Patch Embedding Loss: ", (abs(out.numpy() - out_pt.numpy())).mean())
+
+
 
 if __name__ == '__main__':
     print("Test generating axial frequencies")
