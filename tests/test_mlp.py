5c636ef75d1fcbadc8d26a9ab97b330e2bab3c30
nu1ptr <j6quach@gmail.com>
2025-09-18 19:53:54 -0500

    Full mlp tvm conversion

--- tests/test_mlp.py
+++ tests/test_mlp.py
@@ -5,10 +5,19 @@ from typing import OrderedDict
 import tvm
 import numpy as np
 import torch.nn as nn
+import torch
 
+from tir_kernels.mlp import mlp
 from utils import print_diff, get_tensors
 
 def test_mlp(n=1,seq=1024,width=1596, mlp_width=1024):
+    # Init input/output
+    np_x = np.random.uniform(size=(n,seq,width)).astype("float32")
+    tvm_x, pt_x = get_tensors(np_x)
+
+    np_out = np.zeros_like(np_x).astype("float32")
+    tvm_out, _ = get_tensors(np_out)
+
     # Init Torch Module
     pt_mlp = nn.Sequential(
         OrderedDict(
@@ -31,6 +40,17 @@ def test_mlp(n=1,seq=1024,width=1596, mlp_width=1024):
     tvm_c_fc_b, _ = get_tensors(pt_c_fc_b.detach().numpy().astype("float32"))
     tvm_c_proj_w, _ = get_tensors(pt_c_proj_w.detach().numpy().astype("float32"))
     tvm_c_proj_b, _ = get_tensors(pt_c_proj_b.detach().numpy().astype("float32"))
+
+    # Init TVM Module
+    tvm_mlp_mod = tvm.IRModule({'mlp': mlp})
+    tvm_mlp = tvm.build(tvm_mlp_mod, target="llvm")
+
+    # Test
+    with torch.no_grad():
+        pt_out = pt_mlp(pt_x)
+        tvm_mlp(tvm_x, tvm_c_fc_w, tvm_c_fc_b, tvm_c_proj_w, tvm_c_proj_b, tvm_out)
+
+        print_diff(pt_out.numpy(), tvm_out.numpy())
 
 if __name__ == '__main__':
     test_mlp()
--- tir_kernels/mlp.py
+++ tir_kernels/mlp.py
@@ -1,6 +1,6 @@
 """
     This is a TVM implementation of the MLP module as seen
-    in Perception Encoder's Residual Attention Block.
+    in Perception Encoder's Residual Attention Block. It comes after the MHSA.
 
     Which looks like this:
         self.mlp = nn.Sequential(
@@ -14,7 +14,52 @@
 
         Might have to fuse gelu instead of using a separate tir kernel.
 """
+import math
+
 from tvm.script import tir as T
+
 @T.prim_func
-def mlp():
-    return
+def mlp(x: T.handle, c_fc_w: T.handle, c_fc_b: T.handle, c_proj_w: T.handle, c_proj_b: T.handle, out: T.handle):
+    n, seq, width, mlp_width = T.int32(), T.int32(), T.int32(), T.int32()
+
+    X = T.match_buffer(x, [n, seq, width], "float32")
+    C_FW_W = T.match_buffer(c_fc_w, [mlp_width, width], "float32")
+    C_FW_B = T.match_buffer(c_fc_b, [mlp_width], "float32")
+    C_PROJ_W = T.match_buffer(c_proj_w, [width, mlp_width], "float32")
+    C_PROJ_B = T.match_buffer(c_proj_b, [width], "float32")
+
+    OUT_1 = T.alloc_buffer([n,seq,mlp_width], "float32")
+    OUT_2 = T.match_buffer(out, [n,seq,width], "float32")
+
+    for n, s, mw, w in T.grid(n, seq, mlp_width, width):
+        with T.block("mlp_c_fc_w"):
+            vn, vs, vmw, vw = T.axis.remap("SSSR", [n,s,mw,w])
+
+            with T.init():
+                OUT_1[vn,vs,vmw] = T.float32(0)
+            OUT_1[vn,vs,vmw] += X[vn,vs,vw] * C_FW_W[vmw, vw] 
+
+    for n,s,mw in T.grid(n,seq,mlp_width):
+        with T.block("mlp_c_fc_b"):
+            vn, vs, vmw = T.axis.remap("SSS", [n,s,mw])
+            OUT_1[vn,vs,vmw] += C_FW_B[vmw]
+
+    for n, s, mw in T.grid(n,seq,mlp_width):
+        with T.block("mlp_gelu"):
+            vn, vs, vmw = T.axis.remap("SSS", [n,s,mw])
+            pi = T.float32(math.pi)
+            OUT_1[vn, vs, vmw] = 0.5 * OUT_1[vn, vs, vmw] * ( 1 + T.tanh(T.sqrt(2/pi)*(OUT_1[vn,vs,vmw] + 0.044715*T.pow(OUT_1[vn,vs,vmw],3))))
+
+    for n, s, w, mw in T.grid(n, seq, width, mlp_width):
+        with T.block("mlp_c_proj_w"):
+            vn, vs, vw, vmw = T.axis.remap("SSSR", [n,s,w,mw])
+
+            with T.init():
+                OUT_2[vn,vs,vw] = T.float32(0)
+            OUT_2[vn,vs,vw] += OUT_1[vn,vs,vmw] * C_PROJ_W[vw, vmw] 
+
+    for n, s, w in T.grid(n, seq, width):
+        with T.block("mlp_c_proj_b"):
+            vn, vs, vw = T.axis.remap("SSS", [n,s,w])
+
+            OUT_2[vn, vs, vw] += C_PROJ_B[vw]
